5a446afc3460627d867f4f1e063cc288256c9b51
Joe Wasson <joe@sendowl.com>
2023-08-24 17:10:47 -0700

    fix: fix issue where breakpoit srcset_options weren't being used

--- lib/imgix/rails/tag.rb
+++ lib/imgix/rails/tag.rb
@@ -16,16 +16,16 @@ class Imgix::Rails::Tag
 
 protected
 
-  def srcset(source: @source, path: @path, url_params: @url_params, srcset_options: @srcset_options, tag_options: @tag_options)
+  def srcset(source: @source, path: @path, url_params: @url_params, srcset_options: @srcset_options)
     params = url_params.clone
 
     width_tolerance = ::Imgix::Rails.config.imgix[:srcset_width_tolerance]
-    min_width = @srcset_options[:min_width]
-    max_width = @srcset_options[:max_width]
-    widths = @srcset_options[:widths]
-    disable_variable_quality = @srcset_options[:disable_variable_quality]
-    options = { widths: widths, width_tolerance: width_tolerance, min_width: min_width, max_width: max_width, disable_variable_quality: disable_variable_quality}
+    min_width = srcset_options[:min_width]
+    max_width = srcset_options[:max_width]
+    widths = srcset_options[:widths]
+    disable_variable_quality = srcset_options[:disable_variable_quality]
+    options = {widths: widths, width_tolerance: width_tolerance, min_width: min_width, max_width: max_width, disable_variable_quality: disable_variable_quality}
 
-    ix_image_srcset(@source, @path, params, options)
+    ix_image_srcset(source, path, params, options)
   end
 end
